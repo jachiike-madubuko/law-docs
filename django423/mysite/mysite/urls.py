2dcf5253fe278a9c41ba3a1fecc612cc9d2e470b
Justin O'Dea <justinodea@Justins-MacBook-Pro.local>
2018-02-02 10:55:49 -0700

    added contact, blog, and admin page.

--- django423/mysite/mysite/urls.py
+++ django423/mysite/mysite/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    #url(r'^webapp/', include('webapp.urls')),
     url(r'^', include('attorney.urls')),
+    url(r'^blog/', include('blog.urls')),
 ]
